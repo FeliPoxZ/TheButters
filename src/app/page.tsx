eadff46b0591992f073377d473e9abe151aeed30
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2025-11-17 12:30:35 -0300

    Modifying nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,20 @@ import CategoryNavBar from "@/components/ui/cardapio/CategoryNavBar";
 import Header from "@/components/ui/cardapio/Header";
 
 export default function Home() {
-	const categories = ["Cookies Clássicos", "Big Cookies", "Cheesecakes", "Lanches", "Bebidas"]
+	const categories = ["Cookies Clássicos", "Big Cookies", "Cheesecakes", "Lanches", "Bebidas"];
+	const items = Array.from({ length: 20 });
 	return (
 		<ColumnView className="min-h-screen w-dvw select-none">
 			<Header />
-			<CategoryNavBar categories={categories}/>
+			<CategoryNavBar categories={categories} />
+			{categories.map((category) => (
+				<div key={category}>
+					<h2 id={category} className="scroll-mt-14">{category}</h2>
+					{items.map((_, itensIndex) => (
+						<p key={`${category} ${itensIndex}`}>a</p>
+					))}
+				</div>
+			))}
 		</ColumnView>
 	);
 }
--- src/components/ui/cardapio/CategoryNavBar.tsx
+++ src/components/ui/cardapio/CategoryNavBar.tsx
@@ -1,30 +1,47 @@
-import RowView from "@/components/layout/RowView";
-import { cn } from "@/lib/utils";
+"use client";
+
+import { useKeenSlider } from "keen-slider/react";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
+type Category = string;
 
 interface Props {
-	categories: string[];
+	categories: Category[];
 }
 
-function CategoryNavBar({ categories }: Props) {
+export default function CategoryNavBar({ categories }: Props) {
+	const [selectedCategory, setSelectedCategory] = useState<Category>("");
+
+	const [sliderRef] = useKeenSlider<HTMLDivElement>({
+		mode: "free",
+		rubberband: true,
+		slides: {
+			perView: "auto",
+			spacing: 24,
+		},
+	});
+
 	return (
-		<nav className="bg-primary min-w-full py-2 sticky -top-px pl-5 md:pl-11 shadow-md overflow-x-auto">
-			<RowView align="center" className="gap-6">
-				{categories.map((category, i) => (
-					<Link
-						className={cn(
-							"text-item text-lg md:text-xl transition-all hover:scale-105 duration-200 shrink-0",
-							i === 0 ? "text-highlight font-medium" : ""
-						)}
-						key={category}
-						href={""}
-					>
-						{category}
-					</Link>
+		<nav className="bg-primary py-2 sticky -top-px pl-5 md:pl-11 shadow-md w-full">
+			<div ref={sliderRef} className="keen-slider overflow-visible">
+				{categories.map((category, i, arr) => (
+					<div key={category} className="keen-slider__slide w-auto! min-w-max!">
+						<Link
+							href={`#${category}`}
+							onClick={() => setSelectedCategory(category)}
+							className={cn(
+								"text-item text-lg md:text-xl transition-all hover:scale-105 duration-200 shrink-0 whitespace-nowrap",
+								selectedCategory === category && "text-highlight font-semibold",
+								i === arr.length - 1 && "mr-5"
+							)}
+						>
+							{category}
+						</Link>
+					</div>
 				))}
-			</RowView>
+			</div>
 		</nav>
 	);
 }
-
-export default CategoryNavBar;
