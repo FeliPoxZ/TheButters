ad4ca9e6e1ab141f0e6cc627518f39be1039bcca
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2025-12-01 19:14:36 -0300

    Header Fix

--- src/components/common/CommonHeader.tsx
+++ src/components/common/CommonHeader.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import RowView from "../layout/RowView";
 import Line from "./Line";
 import { ReactNode } from "react";
+import { cn } from "@/lib/utils";
 
 interface Props {
 	extra?: ReactNode
@@ -18,7 +19,7 @@ function CommonHeader({extra}: Props) {
 				</div>
 				<section className="w-full px-6">
 					<RowView justify={extra? "between" : "start"} align="center" className="h-full py-3">
-						<div className="flex flex-col sm:flex-row gap-1 sm:gap-2 items-center">
+						<div className={cn(extra? "flex-col" : "flex" ,"flex  sm:flex-row gap-1 sm:gap-2 items-center")}>
 							<div className="relative size-20 md:size-26 border-2 border-white  rounded-full bg-primary -mt-10 md:-mt-16">
 								<Image src={"/Logo.webp"} alt="logo" fill priority className="object-contain" />
 							</div>
